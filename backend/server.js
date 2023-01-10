0ab997ffb9c51812ab52f718bdca20be7ad6d403
Chan Zhe Xiang <czhexg1@gmail.com>
2023-01-10 08:01:21 +0800

    close socket connection

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,7 @@ connectDB().then(() => {
         });
 
         socket.on("typing", (room) => socket.in(room).emit("typing"));
+
         socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
         socket.on("new message", (newMessageReceived) => {
@@ -84,6 +85,11 @@ connectDB().then(() => {
                         .emit("message received", newMessageReceived);
                 }
             }
+        });
+
+        socket.off("setup", () => {
+            console.log("USER DISCONNECTED");
+            socket.leave(userData._id);
         });
     });
 });
